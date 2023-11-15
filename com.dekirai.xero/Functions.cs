c0e3ca5308732e1fac8a5cba0cebb181acbb9ce7
Dekirai <keivo1997@gmail.com>
2023-11-15 09:27:31 +0100

    Added Xero Plugin

---
+++ com.dekirai.xero/Functions.cs
@@ -0,0 +1,128 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Memory;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace com.dekirai.xero
+{
+    [PluginActionId("com.dekirai.xero.functions")]
+    public class Functions : PluginBase
+    {
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                return new PluginSettings();
+            }
+
+            [JsonProperty(PropertyName = "functions")]
+            public string Functions { get; set; }
+
+            [FilenameProperty]
+            [JsonProperty(PropertyName = "xeroFunctions")]
+            public string XeroFunctions { get; set; }
+        }
+
+        private readonly PluginSettings Settings;
+        public static Mem mem = new Mem();
+
+        #region Private Members
+
+        private PluginSettings settings;
+
+        #endregion
+        public Functions(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null || payload.Settings.Count == 0)
+            {
+                Settings = PluginSettings.CreateDefaultSettings();
+                Connection.SetSettingsAsync(JObject.FromObject(Settings));
+            }
+            else
+            {
+                Settings = payload.Settings.ToObject<PluginSettings>();
+            }
+        }
+
+        public override void Dispose()
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            GetPID();
+            string processName = "xerogame";
+            Process[] processes = Process.GetProcessesByName(processName);
+            Process xeroGameProcess = processes[0];
+            IntPtr mainWindowHandle = xeroGameProcess.MainWindowHandle;
+            SetForegroundWindow(mainWindowHandle);
+            Thread.Sleep(450);
+            switch (Settings.Functions)
+            {
+                case "0":
+                    // Simulate key presses
+                    SendKeys.SendWait("{F11}");
+                    SendKeys.SendWait("toggle_pvp");
+                    SendKeys.SendWait("{Enter}");
+                    SendKeys.SendWait("{F11}");
+                    break;
+                case "1":
+                    // Simulate key presses
+                    SendKeys.SendWait("{F11}");
+                    SendKeys.SendWait("pos");
+                    SendKeys.SendWait("{Enter}");
+                    SendKeys.SendWait("{F11}");
+                    break;
+                case "2":
+                    // Simulate key presses
+                    SendKeys.SendWait("{F11}");
+                    SendKeys.SendWait("tp");
+                    SendKeys.SendWait("{Enter}");
+                    SendKeys.SendWait("{F11}");
+                    break;
+            }
+        }
+
+        public override void KeyReleased(KeyPayload payload) { }
+
+        public override void OnTick() { }
+
+        public async override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            // New in StreamDeck-Tools v2.0:
+            Tools.AutoPopulateSettings(Settings, payload.Settings);
+
+            // Return fixed filename back to the Property Inspector
+            await Connection.SetSettingsAsync(JObject.FromObject(Settings)).ConfigureAwait(false);
+        }
+
+        private void GetPID()
+        {
+            int pid = mem.GetProcIdFromName("ProjectG");
+            bool openProc = false;
+
+            if (pid > 0) openProc = mem.OpenProcess(pid);
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+        #endregion
+    }
+}
--- com.dekirai.xero/Program.cs
+++ com.dekirai.xero/Program.cs
@@ -1,82 +1,20 @@
-﻿using System;
-using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Timers;
-using StreamDeckCS;
-using StreamDeckCS.EventsReceived;
+﻿using BarRaider.SdTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace com.dekirai.xero
 {
     internal class Program
     {
-        [DllImport("user32.dll")]
-        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
-
-        const int KEYEVENTF_EXTENDEDKEY = 0x1;
-        const int KEYEVENTF_KEYUP = 0x2;
-
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            Plugin plugin = new Plugin(args);
-            await plugin.startPluginAsync();
-        }
+            // Uncomment this line of code to allow for debugging
+            //while (!System.Diagnostics.Debugger.IsAttached) { System.Threading.Thread.Sleep(100); }
 
-        public class Plugin
-        {
-            StreamdeckCore core;
-            //Timer timer;
-
-            // fields
-            //private int numPressed = 0;
-            //private int timerDuration = 2000;
-            //private string context = null;
-
-            public Plugin(string[] args)
-            {
-                core = new StreamdeckCore(args);
-                //timer = new Timer(timerDuration);
-
-                // subscribe to events
-                //timer.Elapsed += Timer_Elapsed;
-                core.KeyUpEvent += Core_KeyUpEvent;
-                core.KeyDownEvent += Core_KeyDownEvent;
-                core.WillAppearEvent += Core_WillAppearEvent;
-
-                //timer.Enabled = true;
-
-            }
-
-            // raises when button is held for timerDuration (2000 ms i.e 2 seconds), resets counter
-            private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-            {
-                //Not used for now
-            }
-            private void Core_KeyDownEvent(object sender, KeyDown e)
-            {
-                switch (e.action)
-                {
-                    case "com.dekirai.xero.test":
-                        break;
-                }
-            }
-
-            // when we release key, stop timer and set title of the key to next num in fibonacci sequence
-            private void Core_KeyUpEvent(object sender, KeyUp e)
-            {
-                //timer.Stop();
-            }
-
-            private void Core_WillAppearEvent(object sender, WillAppear e)
-            {
-                //Not used for now
-            }
-
-            // starts the plugin
-            public async Task startPluginAsync()
-            {
-                await core.Start();
-            }
-
+            SDWrapper.Run(args);
         }
     }
 }
